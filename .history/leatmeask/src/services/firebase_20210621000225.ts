a311c9ec53307ea9b12e336ef13b0427f90427f0
araguaci <araguaci@gmail.com>
2021-06-21 00:45:07 -0300

    aula1 #together

---
+++ .history/leatmeask/src/services/firebase_20210621000225.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "letmeask-54c05.firebaseapp.com",
+  databaseURL: "https://letmeask-54c05-default-rtdb.firebaseio.com",
+  projectId: "letmeask-54c05",
+  storageBucket: "letmeask-54c05.appspot.com",
+  messagingSenderId: "1020441529121",
+  appId: "1:1020441529121:web:834f08f6081c653c3d0c60"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
