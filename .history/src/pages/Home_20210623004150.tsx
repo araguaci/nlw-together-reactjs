fca4745d59cb4ebbdbd6f6123474f391498b2445
araguaci <araguaci@gmail.com>
2021-06-23 01:26:51 -0300

    finalizando aula3

---
+++ .history/src/pages/Home_20210623004150.tsx
@@ -0,0 +1,80 @@
+import { useHistory } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
+import { useState } from 'react';
+import { FormEvent } from 'react';
+import { database } from '../services/firebase';
+
+export function Home() {
+  const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode] = useState('');
+
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle()
+    }
+    
+    history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+  
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Perguntas e respostas" />
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>
+          Aprenda e compartilhe conhecimento com outras pessoas
+        </p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">ou entre em uma sala</div>
+
+          <form onSubmit={handleJoinRoom}>
+            <input 
+              type="text" 
+              placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
