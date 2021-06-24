5e212f6d021840213a7373f848375e122f6e7138
araguaci <araguaci@gmail.com>
2021-06-24 00:51:06 -0300

    conclusão de aula, falta resolver detahes de layout

---
+++ .history/src/App_20210624000511.tsx
@@ -0,0 +1,26 @@
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
